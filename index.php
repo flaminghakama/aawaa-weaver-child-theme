4ee78b35355d9d4059e8c2b6e4f02b7d251ee65c
David Elaine Alt <elaine@flaminghakama.com>
2013-10-04 03:26:17 -0700

    first draft of homepage feature

--- index.php
+++ index.php
@@ -20,7 +20,141 @@ echo("\t<div id=\"main\">\n");
 weaverii_trace_template(__FILE__);
 //weaverii_get_sidebar_left('index');
 ?>
-		<div id="container_wrap"<?php weaverii_get_page_class('index', 'container-index'); ?>>
+		<div id="container_wrap" class="homepage_slideshow">
+
+<!-- BEGIN adapted content for homepage slideshow -->
+
+<?php
+
+function formatFeature($feature_url,$image_url,$title,$description,$excerpt) {
+
+  $html = "" ; 
+  $html .= "<div class='feature'>\n" ; 
+  $html .= "  <div class='feature_left_col'>\n" ; 
+  $html .= "    <div class='media'><a href='$feature_url'><img src='$image_url'></a></div for='media'>\n" ; 
+  $html .= "    <div class='meta'><p><a href='$feature_url'>$title</a></p>\n<p class='description'>$description</p></div for='meta'>\n" ; 
+  $html .= "  </div for='feature_left_col'>\n" ;
+  $html .= "  <div class='excerpt'>$excerpt</div for='excerpt'>\n" ; 
+
+  $html .= "</div for='feature'>\n" ;
+  $html .= "<div class='feature_nav'></div for='feature_nav'>\n" ; 
+
+  return $html ; 
+}
+
+  //  Query for pages with custom variable "Homepage Feature Order" sorted by that variable
+  $querystr = "
+    SELECT $wpdb->posts.* 
+    FROM $wpdb->posts, $wpdb->postmeta
+    WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id 
+    AND $wpdb->postmeta.meta_key = 'Homepage Feature Order' 
+    AND $wpdb->posts.post_status = 'publish' 
+    AND $wpdb->posts.post_type = 'post'
+    ORDER BY $wpdb->postmeta.meta_value ASC
+  ";
+
+  $pageposts = $wpdb->get_results($querystr, OBJECT);
+  global $post; 
+
+  // collect the featured posts
+  $featured = array() ; 
+  $slide_count = 0 ; 
+  // collect the featured nav selectors
+  $selectors = "" ; 
+
+  //                          
+  // loop through the posts.
+  //                          
+  foreach ($pageposts as $post): 
+
+    setup_postdata($post); 
+    $single = true ; 
+
+    // Get the nav 
+    $order = get_post_meta( $post->ID, 'Homepage Feature Order', $single ); 
+    $title = get_the_title() ;
+    $description = get_post_meta( $post->ID, 'Homepage Feature Description', $single ); 
+
+    //echo "<p>title is $title</p>\n" ; 
+
+    if (( $order == "" ) || ( $order < 1 )) { 
+      echo "<!-- omitting homepage feature '$title' since order = '$order' -->\n" ; 
+      continue ; 
+    }
+    echo "<!-- including homepage feature '$title' since order = '$order' -->\n" ; 
+
+    // Get the featured image and excerpt
+    $image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+    $excerpt = $post->post_excerpt ; 
+
+     // Get the URL of the actual featured page 
+    $slug = get_post_meta( $post->ID, 'Homepage Feature Slug', $single ); 
+    $feature_url = get_site_url() . "/$slug" ; 
+
+    // Populate this feature
+    $this_feature['order'] = $order ; 
+    $this_feature['ID'] = $post->ID ;
+    $this_feature['image_url'] = $image_url ;
+    $this_feature['excerpt'] = $excerpt ;
+    $this_feature['slug'] = $slug ;
+    $this_feature['feature_url'] = $feature_url ;
+    $this_feature['order'] = $order ;
+    $this_feature['title'] = $title ;
+    $this_feature['description'] = $description ;
+
+    // Add this feature to the list
+    $slide_count++ ;    
+    $featured[$slide_count] = $this_feature ;
+
+    if ( $slide_count == 1 ) { 
+
+      echo formatFeature($feature_url,$image_url,$title,$description,$excerpt) ; 
+
+      $selectors .= "<div class='selector' id='slide_$slide_count' class='selected'><img src='" . get_stylesheet_directory() . "/images/feature-nav/indicator.png'></div for='slide_$slide_count'>" ; 
+
+    } else { 
+
+      $first = false ; 
+
+      $selectors .= "<div class='selector' id='slide_$slide_count' class='not_selected'><img src='" . get_stylesheet_directory() . "/images/feature-nav/indicator.png'></div for='slide_$slide_count'>" ; 
+    }
+
+  endforeach;
+?>
+
+<script src="<?php echo get_stylesheet_directory_uri() ; ?>/js/jquery-1.6.1.js" type="text/javascript"></script>
+<script language="JavaScript">
+
+$( document ).ready(function() {
+
+  //alert("in document ready function") ; 
+
+<?php 
+  echo "   \$(\"div.feature_nav\").html(\"$selectors\") ;\n" ; 
+
+  foreach ($featured as $slide_count=>$f):  
+
+    echo "// slide_count $slide_count \n" ; 
+
+    $handler = "   \$(\"#slide_$slide_count\").click( \n" . 
+               "     function(){ \n" . 
+               "       alert('in handler for slide $slide_count') ; \n" .
+               "       updateFeature('$f[excerpt]','$f[feature_url]','$f[image_url]','$f[title]','$f[description]') ;\n" . 
+               "       updateNav('$slide_count') ;\n" . 
+               "   }) ;\n" ; 
+
+    echo $handler ; 
+
+  endforeach; 
+
+?>
+}) ; 
+</script>
+
+<!-- END adapted content for homepage slideshow -->
+
+
+
 <?php		if ( weaverii_getopt('wii_infobar_location') == 'content' ) get_template_part('infobar');
 		weaverii_inject_area('precontent'); ?>
 		<div id="container" class="cf index-posts">
