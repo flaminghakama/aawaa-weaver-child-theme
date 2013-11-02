4aa66dc298acf9ec5cad46c56dd1294f8a91aa43
D. Elaine Alt <elaine@flaminghakama.com>
2013-11-02 01:28:03 -0700

    Adding past events page. Updated use of featured images in past events and events page templates.

--- page-past-events.php
+++ page-past-events.php
@@ -1,17 +1,15 @@
-<!-- BEGIN  page-events.php  -->
+<!-- BEGIN  page-past-events.php  -->
 <?php
 /*
- * Template Name: Events
- * Description: Template for the events page.  Also used by the "Current Events" page.
+ * Template Name: Past Events
+ * Description: Template for the past events page.
  *
- * This page will display the content of the events page, 
- * plus excerpts from several other events pages/posts.
+ * This page will display the content of the past events page, 
+ * plus excerpts from all expired events pages/posts.
  *
- * The number of exceprted events is defined in the events page's 
- * custom variable, "How Many Featured Events".
- *  
  * A query is issued to grab the pages/posts that have the custom variable 
- * "Events Expiration Date (YYYY-MM-DD)", sorted in most-recent-first order.
+ * "Events Expiration Date (YYYY-MM-DD)", sorted in most-recent-first orde, 
+ * and filtered to exclude any events that have not yet expired.
  *
  * @package WordPress
  * @subpackage Weaver II
@@ -51,6 +49,8 @@ global $post;
 $max_events = get_post_meta($post->ID, 'How Many Featured Events', true);
 //echo "<p>max events for post id $post->ID is $max_events</p>\n" ; 
 
+ $today = date('Y-m-d') ; 
+
  $querystr = "
     SELECT $wpdb->posts.* 
     FROM $wpdb->posts, $wpdb->postmeta
@@ -66,12 +66,25 @@ $max_events = get_post_meta($post->ID, 'How Many Featured Events', true);
 
  foreach ($pageposts as $post):
    setup_postdata($post);
+
    $this_title = get_the_title() ;
-   $this_permalink = get_permalink() ;
+   //echo "<p>Looking at post <b>$this_title</b></p>\n" ; 
+ 
+   //  Make sure this event has expired.
+   //  Compare the expiration date with today.
+   //  If the expiration is greater than today, it has not yet expired.
+   $this_expiration = get_post_meta($post->ID, 'Events Expiration Date (YYYY-MM-DD)', true) ; 
+   if ( strcmp($this_expiration, $today) >= 0 ) { 
+     echo "<!-- omitting current event $this_title which expires $this_expiration -->\n" ; 
+     continue ;
+   }  
+
    $image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
    //echo "<p>featured image URL for $this_title is $image_url</p>\n" ; 
    $this_excerpt = get_post_meta($post->ID, 'Events Excerpt', true) ; 
+   $this_permalink = get_permalink( $post->ID ); 
    $events_content .= "<div class='events-excerpt'>\n<a href='$this_permalink'><img src='$image_url' width='200' height='133'></a>\n<div><h3><a href='$this_permalink'>$this_title</a></h3>\n$this_excerpt\n</div>\n</div>\n" ; 
+
 endforeach ; 
  
  echo $events_content ; 
@@ -85,4 +98,4 @@ endforeach ;
 <?php	weaverii_get_sidebar_right('page');
 	weaverii_get_footer('page');
 ?>
-<!-- END  page-events.php  -->
+<!-- END  page-past-events.php  -->
