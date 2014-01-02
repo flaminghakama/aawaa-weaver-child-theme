d64dbf56c7a65cb042a7a8d940614d09db787532
David Elaine Alt <elaine@flaminghakama.com>
2014-01-01 22:26:21 -0800

    Initial add of clone of MAU membership infrasructure:  update-profile theme page plus JS and CSS.

---
+++ js/members/aawaa-admin.js
@@ -0,0 +1,303 @@
+
+MAUAdmin =  window.MAUAdmin || {};
+
+(function() {
+  Element.prototype.triggerEvent = function(eventName)
+  {
+    if (document.createEvent)
+    {
+      var evt = document.createEvent('HTMLEvents');
+      evt.initEvent(eventName, true, true);
+
+      return this.dispatchEvent(evt);
+    }
+
+    if (this.fireEvent) {
+      return this.fireEvent('on' + eventName);
+    }
+  };
+
+  var M = MAUAdmin;
+  M.Roles = {
+    init: function() {
+      /** bind events */
+      var ctrls = $('role_mgr');
+      if (ctrls) {
+        var btn = ctrls.selectOne('.add_userrole');
+        if (btn) {
+          $(btn).observe('click', function() {
+            var frm = ctrls.selectOne('form.edit_role');
+            if (frm) {
+              if (frm.visible()) {
+                frm.hide();
+              } else {
+                frm.show();
+              }
+            }
+          });
+        }
+      }
+    }
+  };
+
+  M.FeaturedArtist = {
+    init: function() {
+      var imFeatured = $$('.featured .artist_info .controls .formbutton');
+      imFeatured = (imFeatured.length ? imFeatured[0] : null);
+      if (imFeatured) {
+        var artist = imFeatured.data('artist');
+        imFeatured.observe('click', function() {
+          var xhr = new Ajax.Request('/artists/' + artist + '/notify_featured',
+                                     { method: 'post',
+                                       parameters: {
+                                         authenticity_token:unescape(authenticityToken)
+                                       },
+                                       onSuccess: function(transport) {
+                                         MAU.Flash.show({notice:'Awesome.  We\'ve sent this artist a note telling them they\'ve been featured.'}, '.singlecolumn .featured');
+                                       },
+                                       onFailure: function(transport) {
+                                         MAU.Flash.show({error:'failed to notify the artist.  please contact your system administrator'}, '.singlecolumn .featured');
+                                       },
+                                       onComplete: function(transport) {
+                                       }
+                                     });
+        });
+      }
+    }
+  }
+  M.init = function() {
+    _.each([M.AdminNav, M.Roles, M.FeaturedArtist], function(modul) {
+      if (modul && modul.init) { modul.init(); }
+    });
+    $$('.hide-rows input').each(function(el) {
+      el.observe('click', function() {
+        var clz = this.value;
+        var show = this.checked;
+        $$('table.admin-table tr.' + clz).each(function(row) {
+          if (show) {
+            row.fade();
+          } else {
+            row.appear();
+          }
+        });
+      });
+    });
+    $$('button.update-artists').each(function(el) {
+      el.observe('click', function() {
+	var oss = $$('.participating_status');
+	var cbs = $$('.cboslive');
+	var ii = 0;
+	var updates = {};
+        if (oss && cbs) {
+	  for ( ; ii < oss.length; ++ii ) {
+	    os = oss[ii];
+	    cb = cbs[ii];
+
+	    if ((os.innerHTML === 'true') !== (cbs[ii].checked)) {
+	      updates["ARTIST"+os.readAttribute('artistid')] = (cbs[ii].checked).toString();
+	    }
+	  }
+        }
+	var form = new Element('form', {
+          action: "/admin/artists/update",
+          method: "post"
+        });
+	form.appendChild(new Element('input', { type:"hidden",
+                                                name:"authenticity_token",
+                                                value:unescape(authenticityToken)}));
+        var k = null;
+	for (k in updates) {
+	  var val = updates[k];
+	  form.appendChild(new Element('input', { type:"hidden", name:k, value:val }));
+	}
+	document.body.appendChild(form);
+	form.submit();
+	return false;
+      });
+    });
+
+    var oscombo = $('os_combo_link');
+    if (oscombo) {
+        oscombo.observe('click', function() {
+          var $frm = $('multi_form');
+          if (!$frm.visible()) {
+            $frm.slideDown();
+          } else {
+            $frm.slideUp();
+          }
+        });
+    }
+
+  };
+  Event.observe(window,'load',M.init);
+
+  var E = M.Events = M.Events || {};
+  E.init = function() {
+    $$('.filters input[type=checkbox]').each(function(lnk) {
+      lnk.observe('change',function() {
+        var time_filters = [];
+        var state_filters = [];
+        $$('.filters input.time[type=checkbox]').each(function(checked) {
+          if (checked.checked) {
+            time_filters.push(checked.getValue());
+          }
+        });
+        $$('.filters input.state[type=checkbox]').each(function(checked) {
+          if (checked.checked) {
+            state_filters.push(checked.getValue());
+          }
+        });
+        $$('.event').each(function(el){el.hide();});
+        state_filters.each(function(state_class) {
+          time_filters.each(function(time_class) {
+            $$( ['.event',time_class, state_class].join('.') ).each(function(el){el.show();});
+          });
+        });
+
+      });
+    });
+    $$('.filters .show_all').each(function(el) {
+      el.observe('click', function() {
+        $$('.filters input[type=checkbox]').each(function(ck) {
+          ck.checked = true;
+          $$('.event').each(function(el){el.show();});
+        });
+      });
+    });
+    // start with all
+    var k = null;
+    var init_state = { future: true,
+                       in_progress: true,
+                       past: true,
+                       published: true,
+                       unpublished: true};
+    for (k in init_state) {
+      var $ev_filter = $('event_filter_'+k);
+      if ($ev_filter) {
+        $ev_filter.checked = init_state[k];
+        $ev_filter.triggerEvent('change');
+      }
+    }
+  };
+  Event.observe(window,'load',E.init);
+
+
+  /** internal email lists */
+  var EL = M.InternalEmailLists = MAUAdmin.InternalEmailLists || {};
+  EL.init = function() {
+    $$('.del_btn').each(function(btn) {
+      $(btn).observe('click', function(ev) {
+        var li = $(btn).firstParentByTagName('li');
+        var ul = $(btn).firstParentByTagName('ul');
+        if (li && ul) {
+          var email = li.firstChild.nodeValue.strip();
+          var email_id = li.getAttribute('email_id');
+          var listname = ul.getAttribute('list_type');
+          if ( email && listname && email_id ) {
+            if (confirm('Whoa Nelly!  Are you sure you want to remove ' + email + ' from the ' + listname + ' list?')) {
+              var data_url = '/email_list/';
+              var ajax = new Ajax.Request(data_url, {
+                method: 'post',
+                parameters: {
+                  authenticity_token:unescape(authenticityToken),
+                  'email[id]': email_id,
+                  listtype: listname,
+                  method: 'remove_email'
+                },
+                onSuccess: function(transport) {
+                  li.remove();
+                }
+              });
+            }
+          }
+        }
+        ev.stop();
+        return false;
+      });
+    });
+    $$('.add_btn').each(function(btn) {
+      $(btn).observe('click', function(ev) {
+        var li = $(btn).firstParentByTagName('li');
+        if (li) {
+          var container = $(li).select('.add_email');
+          if (container) {
+            var c = $(container[0]);
+            if ($(c).visible()) {
+              $(c).slideUp();
+            } else {
+              $(c).slideDown();
+            }
+          }
+        }
+        ev.stop();
+        return false;
+      });
+    });
+  };
+  Event.observe(window, 'load', EL.init);
+
+})();
+
+/* mau admin js */
+
+(function() {
+
+  var date_formatter = function(time_sec) {
+    var t = time_sec * 1000;
+    var dt = new Date(t);
+    return dt.getMonth() + "/" + dt.getDate() + "/" + (''+dt.getFullYear()).substr(2);
+  };
+
+  var xticks = 5;
+  var yticks = 4;
+  var labelColor = "#c6c7d6";
+  var lineColor = "#738294";
+  var tickColor = '#222';
+  var apd_options = {
+    xaxis: { tickFormatter: date_formatter,
+             noTicks: xticks, color: lineColor, max: new Date().valueOf()/1000 },
+    yaxis: { noTicks: yticks, color: lineColor, min: 0},
+    grid: { tickColor: tickColor },
+    series: { color: labelColor }
+  };
+
+  var GraphPerDay = {
+    load: function(selector, dataurl) {
+      var ajax = new Ajax.Request(dataurl, {
+        method: 'get',
+        onSuccess: function(transport) {
+          var json = transport.responseText.evalJSON();
+          if (json.series && json.options) {
+            json.options.bars = {show:true};
+            Object.extend(json.series[0], apd_options.series);
+            if (json.options.xaxis) {
+              Object.extend(json.options.xaxis, apd_options.xaxis);
+            } else {
+              json.options.xaxis = apd_options.xaxis;
+            }
+            if (json.options.yaxis) {
+              Object.extend(json.options.yaxis, apd_options.yaxis);
+            } else {
+              json.options.yaxis = apd_options.yaxis;
+            }
+            if (json.options.grid) {
+              Object.extend(json.options.grid, apd_options.grid);
+            } else {
+              json.options.grid = apd_options.grid;
+            }
+            var $graph = $(selector);
+            $graph.setStyle({display:'block'});
+            f = Flotr.draw($graph, json.series, json.options);
+          }
+        }
+      });
+    }
+  };
+  if (/\/admin$/.test(location.href)) {
+    Event.observe(window, 'load', function() { GraphPerDay.load('artists_per_day', '/admin/artists_per_day'); });
+    Event.observe(window, 'load', function() { GraphPerDay.load('favorites_per_day', '/admin/favorites_per_day'); });
+    Event.observe(window, 'load', function() { GraphPerDay.load('art_pieces_per_day', '/admin/art_pieces_per_day'); });
+    Event.observe(window, 'load', function() { GraphPerDay.load('os_signups', '/admin/os_signups'); });
+  }
+
+})();
---
+++ js/members/aawaa-notes.js
@@ -0,0 +1,410 @@
+/*
+for all elements with 'send-mau-a-note' class, popup a form to fill
+out the appropriate info and fire off a note to mau emails
+
+avoids 'mailto' links
+*/
+
+MAU.NotesMailer = Class.create();
+MAU.browser = MAU.browser || {};
+var FormConstructors = {};
+
+FormConstructors.login_required = {
+  title: "Login Required", 
+  render: function() {
+    var el = new Element('div');
+    var msg = new Element('div');
+    msg.innerHTML = "Sorry, but you need to login first.";
+    var msg2 = new Element('div');
+    msg2.innerHTML = "Click <a href='/login'>here to login.</a>";
+    el.insert(msg);
+    el.insert(msg2);
+    return el;
+  }
+};
+
+FormConstructors.entrythingy = {
+  title: 'Show Submission Help',
+  render: function() {
+    var el = new Element('div');
+    el.innerHTML = "Sorry you're having trouble.  Have you read the EntryThingy instructions?"+
+      "  If not, check those out and see if they answer your question.   If you've already read"+
+      " all that and are still having issues, tell us as much as you can about the specific "+
+      "problem and we'll do our best to help you out.";
+
+    var inputs = new Element('ul');
+    var entries = [];
+    
+    MAU.Cookie.init({name:'mau'});
+    var email = MAU.Cookie.getData('email') || '';
+    entries.push( [
+      new Element('label').update('Email'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email', 
+                                                       name: 'email', 
+                                                       value: email})) 
+    ]);
+    entries.push( [
+      new Element('label').update('Confirm Email'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email_confirm', 
+                                                       name: 'email_confirm', 
+                                                       value: email })) 
+    ]);
+    entries.push( [
+      new Element('label').update('Describe the problem'),
+      new Element('div').insert(new Element('textarea', { columns: 80, 
+                                                          rows: 7, 
+                                                          id: 'inquiry', 
+                                                          name: 'inquiry' })) 
+    ]);
+    entries.push( [ new Element('input', {type: 'submit', value: 'send'}) ]);
+    
+    $(entries).each(function(entry) {
+      var li = new Element('li');
+      $(entry).each(function(chunk) {
+        li.insert(chunk);
+      });
+      inputs.insert(li);
+    });
+    el.insert(inputs);
+    return el;
+  }
+};
+
+FormConstructors.inquiry = {
+  title: 'General Inquiry',
+  render: function() {
+    var el = new Element('div');
+    el.innerHTML = "We love to hear from you.  Please let us know your thoughts,"+
+      " questions, rants.  We'll do our best to respond in a timely manner.";
+
+    var inputs = new Element('ul');
+    var entries = [];
+    
+    MAU.Cookie.init({name:'mau'});
+    var email = MAU.Cookie.getData('email') || '';
+    entries.push( [
+      new Element('label').update('Email'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email', 
+                                                       name: 'email', 
+                                                       value: email})) ]);
+    entries.push( [
+      new Element('label').update('Confirm Email'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email_confirm', 
+                                                       name: 'email_confirm', 
+                                                       value: email })) ]);
+    entries.push( [
+      new Element('label').update('Question'),
+      new Element('div').insert(new Element('textarea', { columns: 80, 
+                                                          rows: 7, 
+                                                          id: 'inquiry', 
+                                                          name: 'inquiry' })) ]);
+    entries.push( [ new Element('input', {type: 'submit', value: 'send'}) ]);
+    
+    $(entries).each(function(entry) {
+      var li = new Element('li');
+      $(entry).each(function(chunk) {
+        li.insert(chunk);
+      });
+      inputs.insert(li);
+    });
+    el.insert(inputs);
+    return el;
+  }
+};
+
+FormConstructors.event_submission = {
+  title: "My Art Show",
+  login_required: true,
+  render: function() {
+    var el = new Element('div');
+    el.innerHTML = "Tell us about your art show or event.  We'll help you plug"+
+      " it by posting it on our Facebook and Twitter.  Please give as much "+
+      "information as you can.";
+
+    var inputs = new Element('ul');
+    var entries = [];
+
+    entries.push( [
+      new Element('label').update('Title'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'eventtitle', 
+                                                       name: 'eventtitle' })) ]);
+    entries.push( [
+      new Element('label').update('Description'),
+      new Element('div').insert(new Element('textarea', { rows: 7, 
+                                                          id: 'eventdesc', 
+                                                          name: 'eventdesc' })) ]);
+    entries.push( [
+      new Element('label').update('Time & Date'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'eventtimedate', 
+                                                       name: 'eventtimedate' })) ]);
+    entries.push( [
+      new Element('label').update('Venue Name'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'eventvenuename', 
+                                                       name: 'eventvenuename' })) ]);
+    entries.push( [
+      new Element('label').update('Venue Address'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'eventaddress', 
+                                                       name: 'eventaddress' })) ]);
+    entries.push( [
+      new Element('label').update('Event Website'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'eventlink', 
+                                                       name: 'eventlink' })) ]);
+    entries.push( [
+      new Element('label').update('Participating MAU Artists (comma separated list)'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'eventartists', 
+                                                       name: 'eventartists' })) ]);
+    entries.push( [ new Element('input', {type: 'submit', value: 'send'}) ]);
+
+    $(entries).each(function(entry) {
+      var li = new Element('li');
+      $(entry).each(function(chunk) {
+        li.insert(chunk);
+      });
+      inputs.insert(li);
+    });
+    el.insert(inputs);
+    return el;
+  }
+};
+
+FormConstructors.feed_submission = {
+  title: "Art Feeds",
+  render: function() {
+    var el = new Element('div');
+    el.innerHTML = "Tell us about your favorite art related feed.  We'll check"+
+      " it out and if it stays current and is interesting, we'll add it to our list.";
+
+    var inputs = new Element('ul');
+    var entries = [];
+
+    entries.push( [
+      new Element('label').update('Feed Link'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'feedlink', 
+                                                       name: 'feedlink' })) ]);
+    entries.push( [ new Element('input', {type: 'submit', value: 'send'}) ]);
+
+    $(entries).each(function(entry) {
+      var li = new Element('li');
+      $(entry).each(function(chunk) {
+        li.insert(chunk);
+      });
+      inputs.insert(li);
+    });
+    el.insert(inputs);
+    return el;
+  }
+};
+
+FormConstructors.help = {
+  title: "Help!",
+  render: function() {
+    var el = new Element('div');
+    el.innerHTML = "Ack.  So sorry you're having issues.  Our developers are only human."+
+      "  You may have found a bug in our system.  Please tell us what you were doing "+
+      "and what wasn't working.  We'll do our best to fix the issue and get you rolling "+
+      "as soon as we can.";
+
+    var inputs = new Element('ul');
+    var entries = [];
+
+    MAU.Cookie.init({name:'mau'});
+    var email = MAU.Cookie.getData('email') || '';
+
+    entries.push( [
+      new Element('label').update('Email'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email', 
+                                                       name: 'email', 
+                                                       value: email})) ]);
+    entries.push( [
+      new Element('label').update('Confirm Email'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email_confirm', 
+                                                       name: 'email_confirm', 
+                                                       value: email })) ]);
+    entries.push( [
+      new Element('label').update('Report your issue'),
+      new Element('div').insert(new Element('textarea', { columns: 80, 
+                                                          rows: 7, 
+                                                          id: 'inquiry', 
+                                                          name: 'inquiry' })) ]);
+    entries.push( [ new Element('input', {type: 'submit', value: 'send'}) ]);
+  
+    $(entries).each(function(entry) {
+      var li = new Element('li');
+      $(entry).each(function(chunk) {
+        li.insert(chunk);
+      });
+      inputs.insert(li);
+    });
+    el.insert(inputs);
+    return el;
+  }
+};
+
+FormConstructors.email_list = {
+  title: 'Join our mailing list',
+  render: function() {
+    var el = new Element('div');
+    el.innerHTML = "Awesome!  We'll notify you of upcoming MAU events.  "+
+      "We hate spam just like you do so the only things you'll be apprised"+
+      " of will be great!  Enter your email twice below and we'll put you "+
+      "on the list.";
+
+    var inputs = new Element('ul');
+    var entries = [];
+
+    MAU.Cookie.init({name:'mau'});
+    var email = MAU.Cookie.getData('email') || '';
+
+    entries.push( [
+      new Element('div').update('Email:'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email', 
+                                                       name: 'email', 
+                                                       value: email })) ]);
+    entries.push( [
+      new Element('div').update('Confirm Email:'),
+      new Element('div').insert(new Element('input', { type: 'text', 
+                                                       id: 'email_confirm', 
+                                                       name: 'email_confirm', value: email })) ]);
+    entries.push( [ new Element('input', {type: 'submit', value: 'send'}) ]);
+    
+    $(entries).each(function(entry) {
+      var li = new Element('li');
+      $(entry).each(function(chunk) {
+        li.insert(chunk);
+      });
+      inputs.insert(li);
+    });
+    el.insert(inputs);
+    return el;
+  },
+  submit: function() {
+  }
+};
+
+Object.extend(MAU.NotesMailer.prototype, {
+  defaults: {
+    url: '/email'
+  },
+  form_builders: FormConstructors,
+  selector: null,
+  _parent_class: function(selector) {
+    if (selector) {
+      return '.send-mau-a-note.mau-notes-container-' + this.options.note_class;
+    } else {
+      return 'send-mau-a-note mau-notes-container-' + this.options.note_class;
+    }
+  },
+  close: function(ev) {
+    $$(this._parent_class(true)).each(function(el) {
+      if (el) {
+        el.fade({duration:0.3, afterFinish: function() {
+          if (el) { 
+            el.remove(); 
+          } 
+        }});
+      }
+    });
+    if (ev) {
+      ev.stopPropagation();
+    }
+    return false;
+  },
+  insert: function(ev) {
+    var xpos = 0, ypos = 0;
+    if (ev) {
+      xpos = ev.pointerX();
+      ypos = ev.pointerY();
+    }
+    var _that = this;
+    if ($$(this._parent_class(true)).length === 0 ) {
+      var note_class = _that.options.note_class;
+      var formbuilder = _that.form_builders[note_class];
+      if (formbuilder.login_required && !_that.options.username) {
+        formbuilder = _that.form_builders.login_required;
+      }
+      var inner = '';
+      if (formbuilder.render) {
+        inner = $(formbuilder.render());
+      }
+      var f = new Element('form', { method: 'post', action: _that.options.url });
+      $(f).insert(new Element('input', { name: 'authenticity_token', 
+                                         type: 'hidden', 
+                                         value: unescape(authenticityToken) }));
+      $(f).insert(new Element('input', { name: 'note_type', 
+                                         type: 'hidden', 
+                                         value: _that.options.note_class }));
+      $(f).insert(new Element('input', { name: 'browser', 
+                                         type: 'hidden', 
+                                         value: MAU.browser.browser + ' ' + MAU.browser.version }));
+      $(f).insert(new Element('input', { name: 'operating_system', 
+                                         type: 'hidden', 
+                                         value: MAU.browser.OS }));
+      $(f).insert(inner);
+      $(f).observe('submit', function(ev) {
+        if (formbuilder.submit) {
+          formbuilder.submit();
+        }
+        f.request({onComplete: function() { _that.close(ev); }});
+        ev.stop();
+        return false;
+      });
+      var h = new Element('div', { "class": 'popup-header' }).update( formbuilder.title );
+      var x = new Element('div', { "class": 'close-btn' }).update('x');
+      h.insert(x);
+      x.observe('click', function(ev) { _that.close(ev); });
+      var c = new Element('div', { "class": 'popup-content' });
+      var m = new Element('div', { "class": 'popup-mailer' });
+      $(m).insert(h).insert(c);
+      $(c).insert(f);
+      var notes = new Element('div', { "class": _that._parent_class() }).insert(m);
+      $$('body')[0].insert(notes);
+      xx = xpos-250;
+      yy = ypos;
+      if (_that.options.note_class == 'feed_submission') {
+        _that.options.xoffset += 250;
+      }
+      if (_that.options.xoffset) {
+        xx += _that.options.xoffset;
+      }
+      if (_that.options.yoffset) {
+        yy += _that.options.yoffset;
+      }
+      xx = (xx < 20) ? 20 : xx;
+      var style = { left: '' + xx + 'px',
+                    top: ''+ yy + 'px' };
+      if (_that.options.note_class == 'feed_submission') {
+          style.bottom = ''+notes.getHeight() + 'px';
+          style.top = null;
+      }
+      notes.setStyle(style);
+    }
+  },
+  initialize: function(selector, opts) {
+    this.options = Object.extend({},this.defaults);
+    this.options = Object.extend(this.options, opts);
+    this.selector = selector;
+    if (this.options.note_class in this.form_builders) {
+      var _that = this;
+      $$(this.selector).each(function(el) {
+        el.observe('click', function(ev) { 
+          _that.insert(ev); 
+          return false;
+        });
+      });
+    }
+  }
+});
